4ba39f0e67b9cb219c26fc60bf30529678fd18d5
Himanshu Tak <himanshu.zeddlabs@gmail.com>
2021-03-02 19:29:39 +0530

    some changes

--- validation/index.js
+++ validation/index.js
@@ -6,9 +6,17 @@ const addUserSchema = Joi.object({
     email: Joi.string().email().required(),
     contactNumber: Joi.number().min(1000000000).max(9999999999).required(),
     gender: Joi.string().min(1).max(6).equal('male', 'female', 'other').required(),
-    role: Joi.number().min(0).max(2).required() 
+    role: Joi.number().min(0).max(2).required()
+})
+
+const addInputFieldSchema = Joi.object({
+    label: Joi.string().min(1).required(),
+    typeOfField: Joi.string().min(1).required(),
+    description: Joi.string().min(0).allow('').allow(null)
+
 })
 
 module.exports = {
     addUserSchema,
+    addInputFieldSchema
 }
