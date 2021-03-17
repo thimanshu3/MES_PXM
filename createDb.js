1aef876a372ffafe377919c32cc44859d9fdb932
Himanshu Tak <himanshu.zeddlabs@gmail.com>
2021-03-17 22:50:24 +0530

    product listing and showing

--- createDb.js
+++ createDb.js
@@ -4,13 +4,13 @@ var con = mysql.createConnection({
     host: "dme-pxm.crdyivm2svkb.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "mes-hanzala",
-    // database: 'dmepxm'
+    database: 'dmepxm'
 });
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query("INSERT INTO `folders` (`id`, `name`, `parentid`) VALUES (1, 'Folder1', 0),(2, 'Folder2', 0),(3, 'Folder 1 sub1', 1),(4, 'Folder 1 sub1.1', 3),(5, 'Folder 1 sub1.2', 3),(6, 'Folder 1 sub1.1.1', 4),(7, 'Folder 1 sub2', 1),(8, 'Folder 1 sub3', 1),(9, 'Folder 1 sub3.1', 8),(10, 'Folder 2 sub1', 2),(11, 'Folder 2 sub1.1', 10),(12, 'Folder 2 sub1.2', 10),(13, 'Folder 2 sub2', 2),(14, 'Folder 3', 0);",
+    con.query("select * from productMetaData",
      function (err, result) {
         if (err) throw err;
             console.log(result);
--- routes/admin/product.js
+++ routes/admin/product.js
@@ -9,10 +9,7 @@ const router = express.Router()
 
 router.get('/all' , async (req,res)=>{
     try{
-        // let finalData = []
-        const result = await productMetaData.findAll()
-        
-        
+        const result = await MySql.query('SELECT productMetaData.id as id, productMetaData.name as name, productMetaData.formId as formId, productMetaData.active as active,productMetaData.Catalogue as catalogueId, productMetaData.CatalogueHierarchy as catalogueHierarchy, productMetaData.productType as productType,productMetaData.createdBy as createdBy, cat.text as catalogue, ch.name as catalogueHierarchy FROM productMetaData inner join Catalogues cat on productMetaData.Catalogue = cat.id inner join CatalogueHierarchies ch on productMetaData.CatalogueHierarchy = ch.id')
         res.render('admin/kktest', { User: req.user, result})
     }
     catch (err) {
@@ -31,16 +28,16 @@ router.get('/:formId/p/:productId', async (req, res) => {
             await Promise.all(component.subComponents.map(async subComponent => {
                 if (subComponent.type == 'sec') {
                     await Promise.all(subComponent.AssignedFields.map(async a => {
-                        const fields = await MySql.query('select inputFields.id as id , inputFields.active as active , inputFields.label as label , inputFields.description as description, inputFields.associatedList as lr,inputTypes.inputType from inputFields INNER JOIN inputTypes on inputTypes.id = inputFields.typeOfField where inputFields.id = ?', { replacements: [a.fieldId] })
+                        const fields = await MySql.query('select inputFields.id as id, inputFields.active as active, inputFields.label as label, inputFields.description as description, inputFields.associatedList as lr, inputTypes.inputType, productData.productId as pid, productData.fieldValue as pfv from inputFields INNER JOIN inputTypes on inputTypes.id = inputFields.typeOfField INNER join productData on productData.fieldId = inputFields.id where inputFields.id = ? and productData.productId = ?', { replacements: [a.fieldId , req.params.productId] })
                         delete a.fieldId
                         a.field = fields[0][0]
+                        
                     }))
                 } else {
                     await Promise.all(subComponent.tabComponents.map(async tabComponent => {
                         await Promise.all(tabComponent.pageContent.map(async page => {
                             await Promise.all(page.AssignedFields.map(async a => {
-                                const fields = await MySql.query('select inputFields.id as id , inputFields.active as active , inputFields.label as label , inputFields.description as description, inputFields.associatedList as lr,inputTypes.inputType from inputFields INNER JOIN inputTypes on inputTypes.id = inputFields.typeOfField where inputFields.id = ?', { replacements: [a.fieldId] })
-                        
+                                const fields = await MySql.query('select inputFields.id as id, inputFields.active as active, inputFields.label as label, inputFields.description as description, inputFields.associatedList as lr, inputTypes.inputType, productData.productId as pid, productData.fieldValue as pfv from inputFields INNER JOIN inputTypes on inputTypes.id = inputFields.typeOfField INNER join productData on productData.fieldId = inputFields.id where inputFields.id = ? and productData.productId = ?', { replacements: [a.fieldId, req.params.productId] })
                                 delete a.fieldId
                                 a.field = fields[0][0]
                             }))
@@ -49,8 +46,8 @@ router.get('/:formId/p/:productId', async (req, res) => {
                 }
             }))
         }))
-        console.log(layout.componets[0].subComponents[0].AssignedFields[0]);
-        res.render('admin/kktest2', { User: req.user, layout, productId: req.params.productId })
+        const meta = await productMetaData.findOne({where:{id: req.params.productId}})
+        res.render('admin/kktest2', { User: req.user, layout, productId: req.params.productId , meta })
     } catch (err) {
         console.error('\x1b[31m%s\x1b[0m', err)
         req.flash('error', 'Something Went Wrong!')
