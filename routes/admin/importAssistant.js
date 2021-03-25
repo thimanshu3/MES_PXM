406bcfbaeccc6eb336d5eee88540c3ccf253364a
garvitsolanki <garvitsolanki5@gmail.com>
2021-03-25 14:29:33 +0530

    show fields dynamically

--- routes/admin/importAssistant.js
+++ routes/admin/importAssistant.js
@@ -3,12 +3,12 @@ const async = require('async')
 
 const { Redis } = require('../../db')
 const { User } = require('../../models')
-
+const { MySql } = require('../../db')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-
-    res.render('admin/importAssistant', { User: req.user })
+    const [fields] = await MySql.query('select inputFields.id as id , inputFields.active as active , inputFields.label as label , inputFields.description as description, inputFields.associatedList as lr, inputTypes.inputType from inputFields INNER join inputTypes on inputTypes.id = inputFields.typeOfField;')
+    res.render('admin/importAssistant', { User: req.user, fields })
 }
 )
 
